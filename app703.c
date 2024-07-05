2ef184963c9033264b9d8b6c335782ab34e7f2c0
FlimFli <peckbboy2@gmail.com>
2024-07-05 15:22:01 +0700

    first commit

---
+++ app703.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+    int choice;
+    float radius, circum, area;
+    printf("1.Circumference of the circle\n");
+    printf("2.Area of the circle\n");
+    printf("Enter your Choic 1 or 2 :");
+    scanf("%d", &choice);
+    printf("Enter radius of the circle :");
+    scanf("%f", &radius);
+    if (choice == 1){
+        circum = 2 * 3.14 * radius; //2*pi*r เส้นรอบวง
+        printf("circumference of the circle = %.2f\n", circum);
+    }
+    else{
+        area = 3.14 * radius * radius;  // pi*r^2: พื้นที่
+        printf("Area of the Circle = %.2f\n", area);
+    }
+}
